3f622f02d5d081aaa53224c468c3fd2ea2c58388
harik8 <khprasaad@gmail.com>
2026-06-13 19:09:36 +0200

    D-130626: Add AdminSSO users to awsing-github role

--- cdk/cdk/s3_stack.py
+++ cdk/cdk/s3_stack.py
@@ -10,7 +10,7 @@ class S3Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        Tags.of(self).add("AWSCDK", "True")
+        Tags.of(self).add("IaC", "true")
 
         tofu_state = s3.Bucket(self, "s3",
             bucket_name=f"{construct_id}",
